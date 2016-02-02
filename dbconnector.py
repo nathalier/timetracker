bc72aba2c1dffdf45b90e5e0996b8acffc1fa5e3
Nathalie Rud <nathalier@gmail.com>
2016-02-02 03:05:10 +0200

    main form is working

--- dbconnector.py
+++ dbconnector.py
@@ -1,88 +1,99 @@
 __author__ = 'Nathalie'
 
+
 import sqlite3
-from time import time, strftime, gmtime
 from os import path
 from contextlib import closing
 
-from tracker import TP_TODAY, TP_THIS_MONTH, TP_THIS_WEEK, TP_YESTERDAY
 
 DB_NAME = 'ttdb.sqlite'
+
 
 def prepare_db():
     if not path.isfile(DB_NAME):
         conn = sqlite3.connect(DB_NAME)
-        with closing(conn.cursor()) as cur:
-            cur.executescript('''
-            CREATE TABLE tasks (
-	            id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	            task_name	TEXT UNIQUE );
+        conn.executescript('''
+        CREATE TABLE task (
+            id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            task_name	TEXT UNIQUE,
+          /*  parent_task_id	INTEGER,
+            category_id	INTEGER */);
 
-            CREATE TABLE time_logs (
-	            task_id	INTEGER NOT NULL,
-	            start_time	INTEGER,
-	            stop_time	INTEGER,
-	            total_time	INTEGER );
+        CREATE TABLE time_log (
+            task_id	INTEGER NOT NULL,
+            start_time	REAL,
+            stop_time	REAL,
+            total_time	REAL,
+	        offline_logged	INTEGER);
 
-	        CREATE INDEX index_start_time ON time_logs (start_time);
+        CREATE INDEX index_start_time ON time_logs (start_time);
 
-	        CREATE TABLE saved_states (
-	            var	TEXT UNIQUE,
-	            val	TEXT );
+        CREATE TABLE saved_state (
+            var	TEXT UNIQUE,
+            val	TEXT );
 
-            ''')
+      /*  CREATE TABLE category (
+            id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            cat_name	TEXT UNIQUE );
+
+        CREATE TABLE memo (
+            id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+            memo	TEXT NOT NULL,
+            time_log_id	INTEGER,
+            task_id	INTEGER NOT NULL );*/
+
+        ''')
         conn.commit()
         conn.close()
 
 
 def init_db():
-    return retrieve_tasks(), retrieve_saved_states()
+    return retrieve_tasks(), retrieve_saved_state()
 
 
 def retrieve_tasks():
     conn = sqlite3.connect(DB_NAME)
-    with closing(conn.cursor()) as cur:
-        cur.execute('''select id, task_name from tasks;''')
-        res = cur.fetchall()
+    res = conn.execute('''select id, task_name from task;''').fetchall()
     conn.close()
     return dict(res)
 
 
-def retrieve_saved_states():
+def retrieve_saved_state():
     conn = sqlite3.connect(DB_NAME)
-    with closing(conn.cursor()) as cur:
-        cur.execute('''select var, val from saved_states;''')
-        res = cur.fetchall()
+    res = conn.execute('''select var, val from saved_state;''').fetchall()
     conn.close()
     return dict(res)
+
 
 def save_cur_state(vars):
     conn = sqlite3.connect(DB_NAME)
-    with closing(conn.cursor()) as cur:
-        for pair in vars:
-            cur.execute('''insert or replace into saved_states (var, val) values
-                       ( (select var from saved_states where var = ?), ?)''', (pair[0], pair[1]))
-    conn.commit()
+    try:
+        with conn:
+            for pair in vars:
+                conn.execute('''insert or replace into saved_state (var, val) values
+                       ( (select var from saved_state where var = ?), ?)''', (pair[0], pair[1]))
+            # conn.commit()
+    except sqlite3.IntegrityError:
+        pass  # TODO show error message
     conn.close()
-    # 'last_task_id', str(self.current_task_id))
+    # 'last_task_id' #TODO list of parameters name
 
-def log_time(task_id, start_time, stop_time):
+
+def log_time(task_id, start_t, stop_t, total_t, offline_l = 0):
     conn = sqlite3.connect('ttdb.sqlite')
-    cur = conn.cursor()
-    cur.execute('''insert into time_logs (task_id, start_time, stop_time, start_day, total_time)
-                   values (?, ?, ?, ?, ?);''',
-                (task_id, start_time, stop_time, strftime('%Y-%m-%d', gmtime(start_time)), stop_time - start_time))
-    conn.commit()
-    cur.close()
+    try:
+        with conn:
+            conn.execute('''insert into time_log (task_id, start_time, stop_time, total_time, offline_logged)
+                       values (?, ?, ?, ?, ?);''',
+                    (task_id, start_t, stop_t, total_t, offline_l))
+    except sqlite3.IntegrityError:
+        pass  # TODO show error message
+    conn.close()
 
 
-def select_time(task_id, period_start=TP_TODAY, period_end=TP_TODAY):
-    cur = sqlite3.connect('ttdb.sqlite').cursor()
-    if period_start == TP_TODAY:
-        start_day, end_day = strftime('%Y-%m-%d', gmtime()), strftime('%Y-%m-%d', gmtime())
-    #     TODO
-    cur.execute('''select total_time from time_logs where task_id = ? and start_day >= ? and start_day <= ?;''',
-                (task_id, start_day, end_day))
-    res = [x[0] for x in cur.fetchall()]
-    cur.close()
-    return sum(res)
+def select_time(task_id, start_t, end_t):
+    conn = sqlite3.connect('ttdb.sqlite')
+    res = conn.execute('''select total_time from time_log where task_id = ? and start_time >= ? and start_time <= ?;''',
+                (task_id, start_t, end_t)).fetchall()
+    res_list = [x[0] for x in res]
+    return sum(res_list)
--- tracker.py
+++ tracker.py
@@ -1,8 +1,9 @@
 __author__ = 'Nathalie'
 
 import sys
-from PyQt4 import QtCore, QtGui
-from time import time, strftime, gmtime
+from PyQt5 import QtCore, QtWidgets
+from datetime import datetime, date, timedelta
+from time import time
 
 from dbconnector import *
 from mainform import Ui_MainWindow
@@ -12,7 +13,9 @@ TP_TODAY = 'Today'
 TP_YESTERDAY = 'Yesterday'
 TP_THIS_WEEK = 'This Week'
 TP_THIS_MONTH = 'This Month'
-TP_LIST = [TP_TODAY, TP_YESTERDAY, TP_THIS_WEEK, TP_THIS_MONTH]
+TP_LAST_7 = 'Last 7 Days'
+TP_LAST_30 = 'Last 30 Days'
+TP_LIST = [TP_TODAY, TP_YESTERDAY, TP_THIS_WEEK, TP_LAST_7, TP_THIS_MONTH, TP_LAST_30]
 TP_DEF = TP_TODAY
 LABEL_START = "Start"
 LABEL_STOP = "Stop"
@@ -23,15 +26,16 @@ TU_HOUR = 60 * TU_MINUTE
 TIMER_TIMEOUT = TU_SECOND
 
 
-class TtForm(QtGui.QMainWindow):
+class TtForm(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
+        QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.task_started = False
         self.timer_start = 0
         self.time_current = 0
-        self.current_task_id = -1   # tuple of two values: [0] - task_id, [1] - task_name
+        self.cur_task_id = -1   
+        self.cur_period = TP_DEF
         prepare_db()
         self.all_tasks, self.saved_states = init_db()
         self.task_combo_init()
@@ -41,6 +45,7 @@ class TtForm(QtGui.QMainWindow):
         self.timer.timeout.connect(self.tick)
         self.ui.startStop_btn.clicked.connect(self.start_btn_clicked)
         self.ui.task_combo.currentIndexChanged.connect(self.new_task_selected)
+        self.ui.tp_combo.currentIndexChanged.connect(self.new_period_selected)
 
     def tick(self):
         self.time_current += TIMER_TIMEOUT
@@ -59,22 +64,30 @@ class TtForm(QtGui.QMainWindow):
         self.ui.startStop_btn.setText(LABEL_STOP)
 
     def stop_tracking(self):
-        log_time(self.current_task_id, int(self.timer_start), int(time()))
+        time_spent = time() - self.timer_start
+        if abs(time_spent - self.time_current) > TIMER_TIMEOUT:
+            if time_spent < 0: time_spent = 0
+            # TODO show error, propose to correct time
+        log_time(self.cur_task_id, self.timer_start, self.timer_start + time_spent, time_spent, offline_l=0)
         self.timer.stop()
         self.time_current = 0
         self.task_started = False
         self.ui.startStop_btn.setText(LABEL_START)
-        self.update_time_for_period()
+        self.update_time_for_cur_period()
 
-    def update_time_for_period(self):
+    def update_time_for_cur_period(self):
         # TODO period definition
-        if self.current_task_id > 0:
-            total_time = select_time(self.current_task_id, TP_TODAY)
+        if self.cur_task_id > 0:
+            start_t, end_t = self.period_to_timestamp(self.cur_period)
+            total_time = select_time(self.cur_task_id, start_t, end_t)
             total_time_lb = self.time_to_str(total_time, TU_SECOND)
         else:
             total_time_lb = '0m'
         self.ui.totalForPeriod_lb.setText(total_time_lb)
-        self.ui.timeFromStart_lb.setText('0m')
+        if self.task_started:
+            self.ui.timeFromStart_lb.setText(self.time_to_str(self.time_current, TIMER_TIMEOUT))
+        else:
+            self.ui.timeFromStart_lb.setText('0m')
 
     def new_task_selected(self):
         if self.task_started:
@@ -84,10 +97,10 @@ class TtForm(QtGui.QMainWindow):
     def proceed_selected_task(self):
         t_name = self.ui.task_combo.currentText()
         if len(t_name) == 0:
-            self.current_task_id = -1
+            self.cur_task_id = -1
         else:
-            self.current_task_id = self.find_task_id_by_name(t_name)
-        self.update_time_for_period()
+            self.cur_task_id = self.find_task_id_by_name(t_name)
+        self.update_time_for_cur_period()
 
     def find_task_id_by_name(self, task_name):
         for id, t_name in self.all_tasks.items():
@@ -105,22 +118,57 @@ class TtForm(QtGui.QMainWindow):
             combo_ind = 0
         self.ui.task_combo.setCurrentIndex(combo_ind)
         self.proceed_selected_task()
-        self.ui.task_combo.setAutoCompletion(True)
         self.ui.task_combo.setEditable(False)
 
     def tp_combo_init(self):
         self.ui.tp_combo.addItems(TP_LIST)
-        ind = self.ui.tp_combo.findText(TP_TODAY)
+        ind = self.ui.tp_combo.findText(self.cur_period)
         if ind >= 0:
             self.ui.tp_combo.setCurrentIndex(ind)
-        self.ui.tp_combo.setAutoCompletion(True)
         self.ui.tp_combo.setEditable(False)
+
+    def new_period_selected(self):
+        new_period = self.ui.tp_combo.currentText()
+        for period in TP_LIST:
+            if new_period == period:
+                self.cur_period = period
+        self.update_time_for_cur_period()
 
     def closeEvent(self, event):
         if self.task_started:
             self.stop_tracking()
-        save_cur_state([('last_task_id', str(self.current_task_id))])
+        save_cur_state([('last_task_id', str(self.cur_task_id))])
         event.accept()
+
+    def named_period_to_gmtime(self, period):
+        if period == TP_TODAY:
+            pass
+            # return ( , time())
+
+    def period_to_timestamp(self, period = TP_DEF):
+        if period == TP_TODAY:
+            start_t = datetime.combine(date.today(), datetime.min.time()).timestamp()
+            end_t = datetime.now().timestamp()
+        elif period == TP_YESTERDAY:
+            start_t = datetime.combine(date.today() - timedelta(days=1), datetime.min.time()).timestamp()
+            end_t = datetime.combine(date.today(), datetime.min.time()).timestamp()
+        elif period == TP_THIS_WEEK:
+            start_t = datetime.combine(date.today() - timedelta(days=date.today().weekday()), datetime.min.time()).timestamp()
+            end_t = datetime.now().timestamp()
+        elif period == TP_THIS_MONTH:
+            start_t = datetime.combine(date.today() - timedelta(days=date.today().day - 1), datetime.min.time()).timestamp()
+            end_t = datetime.now().timestamp()
+        elif period == TP_LAST_7:
+            start_t = datetime.combine(date.today() - timedelta(days=7), datetime.min.time()).timestamp()
+            end_t = datetime.now().timestamp()
+        elif period == TP_LAST_30:
+            start_t = datetime.combine(date.today() - timedelta(days=30), datetime.min.time()).timestamp()
+            end_t = datetime.now().timestamp()
+        else:
+            # TODO SHOW ERROR
+            start_t, end_t = 0, 0
+        return (start_t, end_t)
+
 
 
     def time_to_str(self, time_amount, rounding_unit=TU_5MIN):
@@ -134,13 +182,8 @@ class TtForm(QtGui.QMainWindow):
         return result
 
 
-
-
-
-
-
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     myapp = TtForm()
     myapp.show()
     sys.exit(app.exec_())
